5ffe6e87da6e993673f5f1350c936714fd4c77b5
otakubinge <113184521+otakubinge@users.noreply.github.com>
2022-09-09 19:19:12 +0530

    Update bug.py

--- VIVI/plugins/Robot/bug.py
+++ VIVI/plugins/Robot/bug.py
@@ -45,7 +45,7 @@ async def bug(_, msg: Message):
     thumb = "https://telegra.ph/file/56d1760224589ee370186.jpg"
     
     bug_report = f"""
-**#ʙᴜɢ :** **@anonymous_was_bot**
+**#ʙᴜɢ :** **@SIXTH_H0KAGE**
 
 **ʀᴇᴩᴏʀᴛᴇᴅ ʙʏ :** **{mention}**
 **ᴜsᴇʀ ɪᴅ :** **{user_id}**
