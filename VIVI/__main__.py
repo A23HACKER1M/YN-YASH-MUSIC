68f12b6478f85545337f762eba5b48d726744c27
otakubinge <113184521+otakubinge@users.noreply.github.com>
2022-09-09 18:36:11 +0530

    Update __main__.py

--- VIVI/__main__.py
+++ VIVI/__main__.py
@@ -1,3 +1,8 @@
+#ɪғ ʏᴏᴜ ғᴏᴜɴᴅ ɪɴ ᴀɴʏ ᴇʀʀᴏʀs ᴛʜᴀɴ ᴘʟᴢ ᴄᴏɴᴛᴀᴄᴛ @SIXTH_H0KAGE
+#sᴜᴘᴘᴏʀᴛ :- @kakashi_bots_support
+#ᴜᴘᴅᴀᴛᴇs :- @kakashi_bots_updates
+#ɴᴇᴛᴡᴏʀᴋ :- @Otaku_Binge
+
 import asyncio
 import importlib
 import sys
