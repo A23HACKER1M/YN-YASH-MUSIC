8dfe35887e481c691ec9e122a7cc3b7dfe3dbee6
Siddharth721 <111099110+Siddharth721@users.noreply.github.com>
2022-09-09 10:57:34 +0530

    Update seek.py

--- VIVI/plugins/Robot/seek.py
+++ VIVI/plugins/Robot/seek.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from VIVI import YouTube, app
-from VIVI.core.call import Anon
+from VIVI.core.call import VIV
 from VIVI.misc import db
 from VIVI.utils import AdminRightsCheck, seconds_to_min
 
