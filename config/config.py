5d7948357941c2be42690db4b2ce1dfea915c9c5
otakubinge <113184521+otakubinge@users.noreply.github.com>
2022-09-09 21:08:24 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,9 +43,9 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/kakashi_bots_support")
+    "SUPPORT_CHANNEL", "https://t.me/kakashi_bots_updates")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/kakashi_bots_updates")
+    "SUPPORT_GROUP", "https://t.me/kakashi_bots_group")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
