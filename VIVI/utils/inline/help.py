98d7d8fbabac032f1cccd8367bc35a80699c1a5b
otakubinge <113184521+otakubinge@users.noreply.github.com>
2022-09-09 19:07:32 +0530

    Update help.py

--- VIVI/utils/inline/help.py
+++ VIVI/utils/inline/help.py
@@ -26,57 +26,57 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="ᴀᴅᴍɪɴ",
+                    text="🔷 ᴀᴅᴍɪɴ 🔷",
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text="ᴀᴜᴛʜ",
+                    text="🔷 ᴀᴜᴛʜ 🔷",
                     callback_data="help_callback hb2",
                 ),
                 InlineKeyboardButton(
-                    text="ʙʟᴀᴄᴋʟɪsᴛ",
+                    text="🔷 ʙʟᴀᴄᴋʟɪsᴛ 🔷",
                     callback_data="help_callback hb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="ʙʀᴏᴀᴅᴄᴀsᴛ",
+                    text="🔷 ʙʀᴏᴀᴅᴄᴀsᴛ 🔷",
                     callback_data="help_callback hb4",
                 ),
                 InlineKeyboardButton(
-                    text="ɢʙᴀɴ",
+                    text="🔷 ɢʙᴀɴ 🔷",
                     callback_data="help_callback hb12",
                 ),
                 InlineKeyboardButton(
-                    text="ʟʏʀɪᴄs",
+                    text="🔷 ʟʏʀɪᴄs 🔷",
                     callback_data="help_callback hb5",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="ᴩɪɴɢ",
+                    text="🔷 ᴩɪɴɢ 🔷",
                     callback_data="help_callback hb7",
                 ),
                 InlineKeyboardButton(
-                    text="ᴩʟᴀʏ",
+                    text="🔷 ᴩʟᴀʏ 🔷",
                     callback_data="help_callback hb8",
                 ),
                 InlineKeyboardButton(
-                    text="ᴩʟᴀʏʟɪsᴛ",
+                    text="🔷 ᴩʟᴀʏʟɪsᴛ 🔷",
                     callback_data="help_callback hb6",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="ᴠɪᴅᴇᴏᴄʜᴀᴛs",
+                    text="🔷 ᴠɪᴅᴇᴏᴄʜᴀᴛs 🔷",
                     callback_data="help_callback hb10",
                 ),
                 InlineKeyboardButton(
-                    text="sᴛᴀʀᴛ",
+                    text="🔷 sᴛᴀʀᴛ 🔷",
                     callback_data="help_callback hb11",
                 ),
                 InlineKeyboardButton(
-                    text="sᴜᴅᴏ",
+                    text="🔷 sᴜᴅᴏ 🔷",
                     callback_data="help_callback hb9",
                 ),
             ],
@@ -98,7 +98,7 @@ def help_back_markup(_):
                     text=_["CLOSE_BUTTON"], callback_data=f"close"
                 ),
                 InlineKeyboardButton(
-                    text="• sᴜᴩᴩᴏʀᴛ •", url=f"{SUPPORT_GROUP}"
+                    text="🔷 sᴜᴩᴩᴏʀᴛ 🔷", url=f"{SUPPORT_GROUP}"
                 ),
             ]
         ]
