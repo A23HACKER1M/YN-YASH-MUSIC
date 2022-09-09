0ef4590cf6a2de17205e84c4a7fe795eea385b82
otakubinge <113184521+otakubinge@users.noreply.github.com>
2022-09-09 19:04:06 +0530

    Update play.py

--- VIVI/utils/inline/play.py
+++ VIVI/utils/inline/play.py
@@ -29,7 +29,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     pos = int(y)
 
     line = "—"
-    circle = "◉"
+    circle = "⧁"
 
     bar = line*(pos-1)
     bar += circle
@@ -83,7 +83,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
     pos = int(y)
 
     line = "—"
-    circle = "◉"
+    circle = "⧁"
 
     bar = line*(pos-1)
     bar += circle
