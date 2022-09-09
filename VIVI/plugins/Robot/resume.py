85088656f773f654762d8362deb412d4d1372e1c
otakubinge <113184521+otakubinge@users.noreply.github.com>
2022-09-09 21:07:02 +0530

    Update resume.py

--- VIVI/plugins/Robot/resume.py
+++ VIVI/plugins/Robot/resume.py
@@ -25,7 +25,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Anon.resume_stream(chat_id)
+    await VIV.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention)
     )
